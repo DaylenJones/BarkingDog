379f7c44fca2348bb25caa861ff437de5bf511d0
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-12 12:46:14 -0400

    update

--- src/shouldWakeup.java
+++ src/shouldWakeup.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class shouldWakeup {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(shouldWake(true, 1));
+        System.out.println(shouldWake(false, 2));
+        System.out.println(shouldWake(true, 8));
+        System.out.println(shouldWake(true, -1));
+    }
+
+    private static boolean shouldWake(boolean isBarking, int hourOfDay) {
+        if (hourOfDat < 0) || hourOfDay > 23) {
+    return false;
+        }
+
+        if (isBarking && ( hourOfDay < 8 || hourOfDay > 22)) {
+            return true;
+        }
+    }
+
 }
